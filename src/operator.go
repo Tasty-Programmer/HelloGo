71632c14df9f575855f564d840fe48b965f6a968
komegKim <kimhunsup159@gmail.com>
2021-05-25 07:33:35 +0900

    Operator

---
+++ src/operator.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	c = (a + b) / 5
+	i++
+
+	// + , - , / ,% , * modulus & ++,--
+
+	a == b
+	a != c
+	a >= b
+
+	// == != >=,<=
+
+	A && B
+	A || !(C && B)
+
+	// && ||
+
+	c = (a & b) << 5
+
+	//Bitwise binary AND , OR , XOR , Binary Shift
+
+	a = 100
+	a *= 10 // modulus
+	a >>= 2 // bitwise
+	a |= 1
+
+	// Assignment Operator
+
+	var k int = 10
+	var p = &k  // k address assignment
+	println(*p) // p -> address print
+
+}
